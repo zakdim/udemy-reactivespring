6285b04505c02f3223d91d81e28ea3bc6b074f5f
Dmitri Zakharov <dmitri.zakharov@desjardins.com>
2021-08-27 16:14:04 -0400

    s07-17 Filtering a Reactive Stream

---
+++ learn-reactivespring/src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoFilterTest.java
@@ -0,0 +1,40 @@
+package com.learnreactivespring.fluxandmonoplayground;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Créé par dmitri le 2021-08-27.
+ */
+public class FluxAndMonoFilterTest {
+
+    List<String> names = Arrays.asList("adam", "anna", "jack", "jenny");
+
+    @Test
+    public void filterTest() {
+
+        Flux<String> namesFlux = Flux.fromIterable(names) // adam, anna, jack, jenny
+                .filter(s -> s.startsWith("a"))
+                .log(); // adam, anna
+
+        StepVerifier.create(namesFlux)
+            .expectNext("adam", "anna")
+            .verifyComplete();
+    }
+
+    @Test
+    public void filterTestLength() {
+
+        Flux<String> namesFlux = Flux.fromIterable(names) // adam, anna, jack, jenny
+                .filter(s -> s.length() > 4)
+                .log(); // jenny
+
+        StepVerifier.create(namesFlux)
+                .expectNext("jenny")
+                .verifyComplete();
+    }
+}
