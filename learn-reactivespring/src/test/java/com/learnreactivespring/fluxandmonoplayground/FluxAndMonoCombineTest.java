313614adf275eb5470afac44ccb5846f23440548
Dmitri Zakharov <dmitri.zakharov@desjardins.com>
2021-09-06 22:59:21 -0400

    s07-26 Virtualizing Time in Junit

--- learn-reactivespring/src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoCombineTest.java
+++ learn-reactivespring/src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoCombineTest.java
@@ -3,6 +3,7 @@ package com.learnreactivespring.fluxandmonoplayground;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+import reactor.test.scheduler.VirtualTimeScheduler;
 
 import java.time.Duration;
 
@@ -57,14 +58,21 @@ public class FluxAndMonoCombineTest {
     @Test
     public void combineUsingConcat_withDelay() {
 
+        VirtualTimeScheduler.getOrSet();
+
         Flux<String> flux1 = Flux.just("A", "B", "C").delayElements(Duration.ofSeconds(1));
         Flux<String> flux2 = Flux.just("D", "E", "F").delayElements(Duration.ofSeconds(1));;
 
         Flux<String> mergeFlux = Flux.concat(flux1, flux2);
 
-        StepVerifier.create(mergeFlux.log())
+//        StepVerifier.create(mergeFlux.log())
+//                .expectSubscription()
+//                .expectNext("A", "B", "C", "D", "E", "F")
+//                .verifyComplete();
+        StepVerifier.withVirtualTime(() -> mergeFlux.log())
                 .expectSubscription()
-                .expectNext("A", "B", "C", "D", "E", "F")
+                .thenAwait(Duration.ofSeconds(6))
+                .expectNextCount(6)
                 .verifyComplete();
     }
 
---
+++ learn-reactivespring/src/test/java/com/learnreactivespring/fluxandmonoplayground/VirtualTimeTest.java
@@ -0,0 +1,41 @@
+package com.learnreactivespring.fluxandmonoplayground;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+import reactor.test.scheduler.VirtualTimeScheduler;
+
+import java.time.Duration;
+
+/**
+ * Créé par dmitri le 2021-09-06.
+ */
+public class VirtualTimeTest {
+
+    @Test
+    public void testingWithoutVirtualTime() {
+
+        Flux<Long> longFlux = Flux.interval(Duration.ofSeconds(1))
+                .take(3);
+
+        StepVerifier.create(longFlux.log())
+                .expectSubscription()
+                .expectNext(0L, 1L, 2L)
+                .verifyComplete();
+    }
+
+    @Test
+    public void testingWithVirtualTime() {
+
+        VirtualTimeScheduler.getOrSet();
+
+        Flux<Long> longFlux = Flux.interval(Duration.ofSeconds(1))
+                .take(3);
+
+        StepVerifier.withVirtualTime(() -> longFlux.log())
+                .expectSubscription()
+                .thenAwait(Duration.ofSeconds(3))
+                .expectNext(0L, 1L, 2L)
+                .verifyComplete();
+    }
+}
